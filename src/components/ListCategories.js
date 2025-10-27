3dd70a12317d1b447da20419d6a2e8c7562c2194
Hesa20 <firdaushesa98@gmail.com>
2025-10-27 16:23:10 +0700

    first commit

---
+++ src/components/ListCategories.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { Col, ListGroup } from "react-bootstrap";
+import axios from "axios";
+import { API_URL } from "../utils/constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUtensils,
+  faCoffee,
+  faCheese,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Icon = ({ nama }) => {
+  switch (nama) {
+    case "Makanan":
+      return <FontAwesomeIcon icon={faUtensils} className="me-2 black" />;
+    case "Minuman":
+      return <FontAwesomeIcon icon={faCoffee} className="me-2 black" />;
+    case "Cemilan":
+      return <FontAwesomeIcon icon={faCheese} className="me-2 black" />;
+    default:
+      return <FontAwesomeIcon icon={faUtensils} className="me-2" />;
+  }
+};
+
+export default class ListCategories extends Component {
+  state = {
+    categories: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "categories")
+      .then((res) => {
+        this.setState({ categories: res.data });
+      })
+      .catch((error) => {
+        console.log("Error yaa ", error);
+      });
+  }
+
+  render() {
+    const { categories } = this.state;
+    const { changeCategory, categoriYangDipilih } = this.props;
+
+    return (
+      <Col md={2} className="mt-3">
+        <h4 className="fw-bold text-center mb-3" style={{ letterSpacing: 0 }}>
+          List Kategori
+        </h4>
+        <hr className="mb-3" />
+        <ListGroup variant="flush" style={{ marginLeft: "5px" }}>
+          {categories.map((category) => (
+            <ListGroup.Item
+              key={category.id}
+              onClick={() => changeCategory(category.nama)}
+              className={`d-flex align-items-center py-3 px-3 mb-2 rounded border-0 ${
+                categoriYangDipilih === category.nama
+                  ? "active-clr text-white category-aktif"
+                  : "normal-clr"
+              }`}
+              style={{ cursor: "pointer", transition: "background 0.2s" }}
+            >
+              <Icon nama={category.nama} />
+              <span
+                className="ms-1"
+                style={{ fontSize: "18px", marginLeft: "50px" }}
+              >
+                {category.nama}
+              </span>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Col>
+    );
+  }
+}
---
+++ src/pages/Sukses.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Button, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { API_URL } from "../utils/constants";
+
+export default class Sukses extends Component {
+  componentDidMount() {
+    axios
+      .get(API_URL + "keranjangs")
+      .then((res) => {
+        const keranjangs = res.data;
+        keranjangs.map(function (item) {
+          return axios
+            .delete(API_URL + "keranjangs/" + item.id)
+            .then((res) => console.log(res))
+            .catch((error) => console.log(error));
+        });
+      })
+      .catch((error) => {
+        console.log("Error yaa ", error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="mt-5 text-center">
+        <Image
+          src="assets/images/success.png"
+          className="success-img"
+          width="500px"
+        />
+        <h2 style={{ marginTop: "20px", fontWeight: "bold" }}>
+          Pesanan Berhasil
+        </h2>
+        <h5>Terimakasih Sudah Memesan!</h5>
+        <Button
+          as={Link}
+          to="/"
+          className="px-4 py-2 mt-3"
+          style={{
+            border: "none",
+            fontSize: "17px",
+            backgroundColor: "#47422e",
+          }}
+        >
+          Kembali
+        </Button>
+      </div>
+    );
+  }
+}
