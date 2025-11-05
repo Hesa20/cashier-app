72c72322d080eafa3eb6a7f65c7b247a694cb2ab
Hesa Firdaus <109150746+Hesa20@users.noreply.github.com>
2025-11-05 10:47:25 +0000

    chore: add Netlify configuration for build and redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Tambahkan ini untuk static export
+  images: {
+    unoptimized: true, // Required untuk static export
+  },
   // Proxy `/api/*` to the Hapi backend server during development
   async rewrites() {
     return process.env.NODE_ENV === 'development'
